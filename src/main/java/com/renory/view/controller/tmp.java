bac7e6757a2b10165fd5c36cd16babb2e52aa40e
smit <smit@debian-by-smit>
2018-05-30 13:06:34 +0300

    extemded

---
+++ src/main/java/com/renory/view/controller/tmp.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.renory.view.controller;
+
+import com.renory.model.dao.*;
+
+/**
+ *
+ * @author smit
+ */
+public class tmp {
+    
+}
