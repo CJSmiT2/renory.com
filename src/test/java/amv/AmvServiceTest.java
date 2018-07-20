7371111f3fd55d9cd2e2c3cf17bdc170d32e07d9
smit <smit@debian-by-smit>
2018-07-20 15:44:35 +0300

    start develop controllers. Added security tests

--- src/test/java/amv/AmvServiceTest.java
+++ src/test/java/amv/AmvServiceTest.java
@@ -5,16 +5,22 @@
  */
 package amv;
 
+<<<<<<< OURS
 import com.renory.config.GeneralConfig;
 import com.renory.model.entity.amv.Amv;
+=======
+>>>>>>> THEIRS
 import com.renory.service.AmvService;
 import com.renory.service.impl.AmvServiceImpl;
+<<<<<<< OURS
 import com.renory.view.dto.AmvBaseInfoDto;
 import com.renory.util.FilesUtil;
 import content.AmvContentCreater;
 import java.io.File;
 import org.junit.After;
 import static org.junit.Assert.assertEquals;
+=======
+>>>>>>> THEIRS
 import org.junit.Test;
 
 /**
@@ -23,33 +29,11 @@ import org.junit.Test;
  */
 public class AmvServiceTest {
     private final AmvService amvService = new AmvServiceImpl();
-    final int userId = 1;
-    final int animeSrc = 2;
-    final String title = "test title";
-    
     
     @Test
     public void createAmv(){
-        String alias = amvService.createAmv(title, userId);
-        assertEquals(true, !alias.isEmpty());
-    }
-    
-    @Test
-    public void delete(){
-        String alias = amvService.createAmv(title, userId);
-        Amv amv = amvService.get(alias);
-        amvService.delete(amv.getId());
-        assertEquals(false, amvService.isAmvExist(amv.getId()));
-    }
-    
-    @Test
-    public void updateBaseInfo(){
-        AmvBaseInfoDto baseInfo = AmvContentCreater.baseInfo();
-        String alias = amvService.createAmv(title, userId);
-        Amv amv = amvService.get(alias);
-        baseInfo.setId(amv.getId());
-        amvService.updateBaseInfo(baseInfo);
         
+<<<<<<< OURS
         Amv amv2 = amvService.get(amv.getId());
         
         assertEquals(true, amv2.getDescription().equals(baseInfo.getDescription()));
@@ -76,5 +60,7 @@ public class AmvServiceTest {
     public void clearDb(){
         amvService.clearDb();
         FilesUtil.deleteAllInFolder(new File(GeneralConfig.AMV_FOLDER));
+=======
+>>>>>>> THEIRS
     }
 }
